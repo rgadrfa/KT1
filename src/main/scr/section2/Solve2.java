2f103ac5cc35e58843c2db9d15bafbb59d33b2ea
Ramilka <102136743+rgadrfa@users.noreply.github.com>
2025-09-23 19:58:32 +0500

    Remove method from section2: Solve2

--- src/main/scr/section2/Solve2.java
+++ src/main/scr/section2/Solve2.java
@@ -13,10 +13,4 @@ public class Solve2 {
         if (number < 0) System.out.println(-1); // - Отрицательный знак, если полученное значение меньше чем 0
         else  System.out.println(1); // - Иначе Положительный знак
     }
-
-    // - Решение через метод
-    public static int getDigitalValue(double value){
-        if (value >= 0) return 1; // - если значение больше или равно 0, то знак положительный
-        return -1; // - иначе знак отрицательный
-    }
 }
